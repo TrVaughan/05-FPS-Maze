701aed4425bbe6d449907426d9ba4eff3fa6d788
TrVaughan <trvaugha@iu.edu>
2021-04-15 11:14:02 -0400

    A

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -10,5 +10,5 @@ func _on_Enemy_body_entered(body):
 		var sound = get_node_or_null("/root/Game/Robot")
 		if sound.playing != null:
 			sound.playing = true
-		queue_free()
+		var _scene = get_tree().change_scene("res://UI/LoseScene.tscn")
 	
--- UI/InputRemapMenu.gd
+++ UI/InputRemapMenu.gd
@@ -1,4 +1,5 @@
 extends Control
 
 func _on_Quit_pressed():
+	Global.save_input()
 	get_tree().quit()
